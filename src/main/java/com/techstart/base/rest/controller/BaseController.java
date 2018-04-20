39e378cc7277844170867a53da561131071ecf6b
mjawath <mjawath@gmail.com>
2018-04-20 16:23:07 +0530

    logger

--- src/main/java/com/techstart/base/rest/controller/BaseController.java
+++ src/main/java/com/techstart/base/rest/controller/BaseController.java
@@ -1,5 +1,7 @@
 package com.techstart.base.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.ui.ModelMap;
@@ -13,7 +15,7 @@ public class BaseController {
 
     protected static boolean SHOW_CRUD_LIST = true;
 
-
+    protected  Logger logger;
 
     @Autowired
     MessageSource messageSource;
